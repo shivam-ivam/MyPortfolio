7e7604c4eb7e1c9b35eb3a211eb93624d00074db
Shivam-ivam1 <sdjfjs>
2022-12-05 18:41:44 +0530

    about page added

---
+++ src/script.js
@@ -0,0 +1,11 @@
+// ------- side bar open and close functioning 
+
+function openLeftMenu() {
+    document.getElementById("leftMenu").style.display = "block";
+  }
+
+  function closeLeftMenu() {
+    document.getElementById("leftMenu").style.display = "none";
+  }
+
+// -------------------------------
