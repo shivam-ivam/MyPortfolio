3dd3d33bd36db1ddff1b2d7ffbf46fb7279e146f
Shivam-ivam1 <sdjfjs>
2022-12-14 19:26:54 +0530

    added timline card colapse work

--- src/script.js
+++ src/script.js
@@ -15,3 +15,10 @@ function openLeftMenu() {
 function contactDrop(){
   document.querySelector(".contact-drop-box").classList.toggle("drop-down");
 }
+
+// timeline cards drop down work
+function timelineColap(params) {
+  const cards = document.querySelectorAll(".timeline-card");
+  cards[params].classList.toggle("timeline-card-uncolapse");
+  
+}
