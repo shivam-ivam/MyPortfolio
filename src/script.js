22415210dd47a59fe6704b5e9d07a2b4a0dab3e8
Shivam-ivam1 <sdjfjs>
2022-12-16 15:07:16 +0530

    timeline modified

--- src/script.js
+++ src/script.js
@@ -22,3 +22,31 @@ function timelineColap(params) {
   cards[params].classList.toggle("timeline-card-uncolapse");
   
 }
+
+// slide show of timeline 
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
+}
